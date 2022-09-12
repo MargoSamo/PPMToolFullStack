56d5dd80c1af3435a723a2a50ed14580e0aba143
MargoSamo <margo_ii@mail.ru>
2022-09-12 17:34:19 +0400

    New project Spring + React + ...

--- src/main/java/com/mariia/ppmtoolfullstack/repositories/ProjectRepository.java
+++ src/main/java/com/mariia/ppmtoolfullstack/repositories/ProjectRepository.java
@@ -1,2 +1,11 @@
-package com.mariia.ppmtoolfullstack.repositories;public interface ProjectRepository {
+package com.mariia.ppmtoolfullstack.repositories;
+
+import com.mariia.ppmtoolfullstack.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
 }
