40d7f9cb4617ed578d77664ccfde81d747a3d5a3
MargoSamo <margo_ii@mail.ru>
2022-10-02 21:19:33 +0400

    new class

--- src/main/java/com/mariia/ppmtoolfullstack/services/ProjectTaskService.java
+++ src/main/java/com/mariia/ppmtoolfullstack/services/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.mariia.ppmtoolfullstack.services;
 
+import com.mariia.ppmtoolfullstack.domain.Backlog;
 import com.mariia.ppmtoolfullstack.domain.ProjectTask;
 import com.mariia.ppmtoolfullstack.repositories.BacklogRepository;
 import com.mariia.ppmtoolfullstack.repositories.ProjectTaskRepository;
@@ -15,16 +16,29 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask(){
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+        //Exceptions: Project not found
+
         //PTs to be added to specific project, project != null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
         //set the bl to pt
+        projectTask.setBacklog(backlog);
         // we want our project sequence to be like this: IDPRO-1 IDPRO-2 .... 100 101
+        Integer BacklogSequence = backlog.getPTSequence();
         // Update the BL SEQUENCE
-
+        BacklogSequence++;
+        //Add Sequence to Project task
+        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+        projectTask.setProjectIdentifer(projectIdentifier);
         //INITIAL priority when priority null
+        if(projectTask.getPriority() == 0 || projectTask.getPriority() == null){
+            projectTask.setPriority(3);
+        }
         // INITIAL status when status is nill
-
-
+        if(projectTask.getStatus() == "" || projectTask.getStatus() == null){
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
     }
-
 }
