6427f7cb787251a472fc721ad0bd1515ad7976f1
MargoSamo <margo_ii@mail.ru>
2022-10-03 18:37:43 +0400

    error processing

--- src/main/java/com/mariia/ppmtoolfullstack/web/BacklogController.java
+++ src/main/java/com/mariia/ppmtoolfullstack/web/BacklogController.java
@@ -1,11 +1,50 @@
 package com.mariia.ppmtoolfullstack.web;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mariia.ppmtoolfullstack.domain.ProjectTask;
+import com.mariia.ppmtoolfullstack.services.MapValidationErrorService;
+import com.mariia.ppmtoolfullstack.services.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/backlog")
 @CrossOrigin
 public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                            BindingResult result, @PathVariable String backlog_id){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null)
+            return errorMap;
+
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{backlog_id}")
+    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id){
+
+        return projectTaskService.findBacklogById(backlog_id);
+
+    }
+
+    @GetMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id, pt_id);
+        return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+    }
 }
