0dc0673807c1a1e7554998f81012e96142979337
MargoSamo <margo_ii@mail.ru>
2022-10-12 17:42:09 +0400

    filter to use our tokens

--- src/main/java/com/mariia/ppmtoolfullstack/security/JwtTokenProvider.java
+++ src/main/java/com/mariia/ppmtoolfullstack/security/JwtTokenProvider.java
@@ -1,11 +1,19 @@
 package com.mariia.ppmtoolfullstack.security;
 
 import com.mariia.ppmtoolfullstack.domain.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.mariia.ppmtoolfullstack.security.SecurityConstants.EXPIRATION_TIME;
+import static com.mariia.ppmtoolfullstack.security.SecurityConstants.SECRET;
+
+@Component
 
 public class JwtTokenProvider {
 
@@ -18,10 +26,44 @@ public class JwtTokenProvider {
 
         String userId = Long.toString(user.getId());
 
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", (Long.toString(user.getId())));
+        claims.put("username", user.getUsername());
+        claims.put("fullName", user.getFullName());
+
         return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
     }
 
     //Validate the token
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex){
+            System.out.println("Invalid JWT Signature");
+        } catch (MalformedJwtException ex){
+            System.out.println("Invalid JWT Token");
+        } catch (ExpiredJwtException ex){
+            System.out.println("Expired JWT token");
+        } catch (UnsupportedJwtException ex){
+            System.out.println("Unsupported JWT token");
+        } catch (IllegalArgumentException ex){
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
 
     //get user Id from token
+    public Long getUserIdFromJWT(String token){
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String)claims.get("id");
+
+        return Long.parseLong(id);
+    }
 }
