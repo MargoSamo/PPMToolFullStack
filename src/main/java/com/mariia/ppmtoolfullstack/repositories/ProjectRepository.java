45fedf5422a3dae1c41c2f8c96e59bdb9799cad2
MargoSamo <margo_ii@mail.ru>
2022-10-13 13:59:43 +0400

    read and delete user

--- src/main/java/com/mariia/ppmtoolfullstack/repositories/ProjectRepository.java
+++ src/main/java/com/mariia/ppmtoolfullstack/repositories/ProjectRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends CrudRepository<Project,Long> {
+public interface ProjectRepository extends CrudRepository<Project, Long> {
 //    @Override
 //    Iterable<Project> findAllById(Iterable<Long> iterable);
     Project findByProjectIdentifier(String projectId);
 
     @Override
     Iterable<Project> findAll();
+
+    Iterable<Project> findAllByProjectLeader(String username);
 }
--- src/main/java/com/mariia/ppmtoolfullstack/services/ProjectService.java
+++ src/main/java/com/mariia/ppmtoolfullstack/services/ProjectService.java
@@ -2,9 +2,12 @@ package com.mariia.ppmtoolfullstack.services;
 
 import com.mariia.ppmtoolfullstack.domain.Backlog;
 import com.mariia.ppmtoolfullstack.domain.Project;
+import com.mariia.ppmtoolfullstack.domain.User;
 import com.mariia.ppmtoolfullstack.exceptions.ProjectIdException;
+import com.mariia.ppmtoolfullstack.exceptions.ProjectNotFoundException;
 import com.mariia.ppmtoolfullstack.repositories.BacklogRepository;
 import com.mariia.ppmtoolfullstack.repositories.ProjectRepository;
+import com.mariia.ppmtoolfullstack.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +20,15 @@ public class ProjectService {
     @Autowired
     private BacklogRepository backlogRepository;
 
-    public Project saveOrUpdateProject(Project project) {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Project saveOrUpdateProject(Project project, String username) {
         try {
+
+            User user = userRepository.findByUsername(username);
+            project.setUser(user);
+            project.setProjectLeader(user.getUsername());
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 
             if(project.getId() == null){
@@ -38,27 +48,27 @@ public class ProjectService {
         }
     }
 
-    public Project findByProjectIdentifier(String projectId) {
+    public Project findProjectByIdentifier(String projectId, String username) {
 
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 
         if(project == null) {
             throw new ProjectIdException("Project ID '" + projectId + "' does not exists");
         }
+
+        if(!project.getProjectLeader().equals(username)){
+            throw new ProjectNotFoundException("Project not found in your account");
+        }
+
         return project;
     }
 
-    public Iterable<Project> findAllProjects() {
-        return projectRepository.findAll();
+    public Iterable<Project> findAllProjects(String username) {
+        return projectRepository.findAllByProjectLeader(username);
     }
 
-    public void deleteProjectByIdentifier(String projectId) {
-        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase()); // находим проект по Id идентификатору
+    public void deleteProjectByIdentifier(String projectid, String username) {
 
-        if(project == null) {
-            throw new ProjectIdException("Cannot Project with ID '" + projectId + "' . This project does not exist");
-        }
-
-        projectRepository.delete(project);
+        projectRepository.delete(findProjectByIdentifier(projectid, username));
     }
 }
